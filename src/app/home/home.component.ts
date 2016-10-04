a649fe486cf320588ae9a6b0e559ca651a842332
pbellon <bellon.pierre@gmail.com>
2016-10-04 10:28:30 +0200

    Add navbar component + clean home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataLoaderService } from './sections/charts/data-loader.service';
 
 @Component({
   selector: 'idlmHome',
@@ -7,13 +6,9 @@ import { DataLoaderService } from './sections/charts/data-loader.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    private initialized: boolean = false;
-    constructor(private dataLoader:DataLoaderService){
+    constructor(){
     }
 
     ngOnInit(){
-        this.dataLoader.loadAllData().subscribe((data)=>{
-            this.initialized = true;
-        });
     }
 }
