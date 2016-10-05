87d4f739eedc513d888cd3037783b3a5da560b1d
pbellon <bellon.pierre@gmail.com>
2016-10-05 11:15:16 +0200

    Refactor word-cloud chart to be an autonomous component

---
+++ src/app/home/sections/charts/word-cloud/word-cloud.component.ts
@@ -0,0 +1,91 @@
+import { Component, Input, OnInit, ViewChild, ElementRef, Renderer } from '@angular/core';
+import * as d3_cloud from 'd3-cloud';
+import { select } from 'd3-selection';
+import { scaleLinear, ScaleLinear, scaleSequential, ScaleSequential } from 'd3-scale';
+import { interpolateGreys } from 'd3-scale-chromatic';
+import { extent } from 'd3-array';
+
+import { AbstractChart, sizeType } from '../charts';
+import { DataLoaderService } from '../data-loader.service';
+
+export type wordDataType = {text:string, size:number};
+
+@Component({
+  selector: 'idlmWordCloud',
+  templateUrl: './word-cloud.component.html',
+  styleUrls: ['./word-cloud.component.scss']
+})
+export class WordCloudComponent extends AbstractChart {
+    private cloudLayout:Cloud<cloud.Word>;
+    private words:Array<wordDataType>;
+    private fontScale:ScaleLinear<number,number>;
+    private fillScale:ScaleSequential<any>;
+    @ViewChild('chartPlayground') chartElement: ElementRef;
+    @Input() wordFont:string='Impact';
+    @Input() wordPadding:number=5;
+    @Input() wordRotation:number=0;
+    @Input() dataCatalogKey:string;
+    @Input() ratio:number=0.7;
+
+    constructor(renderer:Renderer, dataLoader:DataLoaderService){
+        super(renderer, dataLoader);
+    }
+
+    updateScales(){
+        let domain = extent(this.words.map((w)=>w.size));
+        this.fontScale = scaleLinear().domain(domain).range([12, 120]);
+        this.fillScale = scaleSequential(interpolateGreys).domain([0, 150]);
+    }
+
+    bindEvents(){
+        this.cloudLayout.on('end', ()=>this.onLayoutDone());
+    }
+
+    initSVG(){
+        let _sizes = this.getSize();
+        let sizes = [_sizes.width, _sizes.height];
+
+        console.log('sizes:', sizes, _sizes);
+        this.cloudLayout = d3_cloud()
+            .size(sizes)
+            .padding(this.wordPadding)
+            .rotate(this.wordRotation)
+            .words(this.words)
+            .fontSize((d)=>this.fontScale(d.size));
+
+        this._svg = select(this.chartElement.nativeElement);
+        this._svg
+            .attr('width',  sizes[0])
+            .attr('height', sizes[1]);
+
+        this._g = this._svg.append('g')
+            .attr('transform', (d)=>`translate(${sizes[0]/2}, ${sizes[1]/2})`);
+    }
+
+    initData(){
+        this.words = this.data.map((d)=>{
+            return {text:d[0], size:d[1]}
+        });
+    }
+
+    getSize():sizeType{
+        let parent = this.chartElement.nativeElement.parentNode.parentNode;
+        let bbox = parent.getBoundingClientRect();
+        return { width: bbox.width, height: bbox.width * this.ratio };
+    }
+
+    draw(){
+        this.cloudLayout.start();
+    }
+    onLayoutDone(){
+        this._g.selectAll('text').data(this.words)
+            .enter().append("text")
+                .style("font-size", (d)=> d.size + "px")
+                .style("font-family", "Arial")
+                .style("fill", (d)=>this.fillScale(d.size + 30))
+                .attr("text-anchor", "middle")
+                .attr("transform", (d)=> `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+                .text(function(d) { return d.text; });
+    }
+
+}
