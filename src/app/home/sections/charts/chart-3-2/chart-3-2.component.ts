6f5a39128bf2c30c08fa23fd098c609e686ffae3
pbellon <bellon.pierre@gmail.com>
2016-10-10 19:05:04 +0200

    Add basic class signature for chart-3-2

--- src/app/home/sections/charts/chart-3-2/chart-3-2.component.ts
+++ src/app/home/sections/charts/chart-3-2/chart-3-2.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer, ElementRef } from '@angular/core';
+import { AbstractChart } from '../charts';
+import { DataLoaderService } from '../data-loader.service';
 
 @Component({
   selector: 'idlmChart-3-2',
   templateUrl: './chart-3-2.component.html',
   styleUrls: ['./chart-3-2.component.scss']
 })
-export class Chart_3_2Component implements OnInit {
+export class Chart_3_2Component extends AbstractChart {
+    dataCatalogKey:string='3.2';
+    chartElement:any=null;
 
-  constructor() { }
+    constructor(renderer:Renderer, dataLoader:DataLoaderService) {
+        super(renderer, dataLoader);
+    }
+    initSVG(){
 
-  ngOnInit() {
-  }
+    }
+    initData(){
+
+    }
+    updateScales(){
+
+    }
+    draw(){
+
+    }
 
 }
