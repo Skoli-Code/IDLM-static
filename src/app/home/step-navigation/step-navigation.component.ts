a7632dc2b673e8fc867970a6bfa09f4a39f20960
pbellon <bellon.pierre@gmail.com>
2016-10-12 18:45:31 +0200

    Fix stepNavigation component
    The component was throwing errors when the active step was changing so
    we added usage of rxjs/BehaviorSubject to mitigate this

--- src/app/home/step-navigation/step-navigation.component.ts
+++ src/app/home/step-navigation/step-navigation.component.ts
@@ -1,73 +1,88 @@
-import { PlatformLocation } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
+// ng imports
+import { AsyncPipe, PlatformLocation } from '@angular/common';
+import { Component, HostListener, OnInit, ChangeDetectionStrategy } from '@angular/core';
+// others libs
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as _ from 'lodash';
 import 'jquery';
 
 @Component({
-  selector: 'idlm-step-navigation',
-  templateUrl: './step-navigation.component.html',
-  styleUrls: ['./step-navigation.component.scss']
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'idlm-step-navigation',
+    templateUrl: './step-navigation.component.html',
+    styleUrls: ['./step-navigation.component.scss']
 })
 export class StepNavigationComponent implements OnInit {
     private steps: NodeListOf<Element>;
-    private activeFragment: string;
+    private activeFragment: BehaviorSubject<string>;
 
-    constructor(private location: PlatformLocation) { }
+    constructor(private location: PlatformLocation){
+    }
 
-    onStepClicked(step){
-        this.activeFragment = step.id;
+    onStepClicked(step) {
+        this.activeFragment.next(step.id);
         this.onHashChange(step.id);
     }
 
     ngOnInit() {
-        this.location.onHashChange((e)=>{
+        this.steps = document.querySelectorAll('.anchor');
+    }
+
+    ngAfterViewInit() {
+        let h = '';
+        this.location.onHashChange((e) => {
             let hash = (e as any).newURL.split('#')[1];
-            this.activeFragment = hash;
+            this.activeFragment.next(hash);
             this.onHashChange(hash);
         });
-        this.steps = document.querySelectorAll('.anchor');
         let hash = this.location.hash.substr(1);
-        if(hash.length > 1){
-            this.activeFragment = hash;
+        if (hash.length > 1) {
+            h = hash;
         }
+        this.activeFragment = new BehaviorSubject<string>(h);
+
     }
 
-    onHashChange(hash){
-        let target = $('#'+hash);
-        $('html,body').stop().animate({scrollTop: target.offset().top }, '400');
+    onHashChange(hash) {
+        let target = $('#' + hash);
+        $('html,body').stop().animate({
+            scrollTop: target.offset().top
+        }, '400');
     }
 
-    isActive(anchor){
-        return this.activeFragment == (anchor.id || null);
+    isActive(anchor) {
+        if(!this.activeFragment){ return false; }
+        return this.activeFragment.getValue() == (anchor.id || null);
     }
 
-    getVisibleAnchor():Element{
+    getVisibleAnchor(): Element {
         let scroll_top = document.body.scrollTop;
-        let _steps = _.filter(this.steps, (s)=> {
+        let _steps = _.filter(this.steps, (s) => {
             let rect = s.getBoundingClientRect();
-            return Math.abs(rect.top) < rect.height/10 || rect.bottom == 0;
+            return Math.abs(rect.top) < rect.height / 10 || rect.bottom == 0;
         });
         return _steps.length ? _steps[0] : null;
     }
 
     @HostListener('window:scroll', ['$event'])
-    onScroll(e){
+    onScroll(e) {
         let activeAnchor = this.getVisibleAnchor();
-        if(activeAnchor && !this.isActive(activeAnchor)){
+        if (activeAnchor && !this.isActive(activeAnchor)) {
             this.setActive(activeAnchor);
             this.updateUrl();
         }
     }
 
-    setActive(el:Element){
-        if(!el){ return; }
-        this.activeFragment = el.id;
+    setActive(el: Element) {
+        if (!el) { return; }
+        this.activeFragment.next(el.id);
     }
 
-    updateUrl(){
-        if(this.activeFragment){
+    updateUrl() {
+        if (this.activeFragment) {
+            let h = this.activeFragment.getValue();
             // this.location.hash = this.activeFragment;
-            this.location.replaceState(this.activeFragment, '', '/#' + this.activeFragment);
+            this.location.replaceState(h, '', '/#' + h);
         }
     }
 }
