0c33f73b1cd744178560ed08aa89fd9f8d125964
pbellon <bellon.pierre@gmail.com>
2016-10-17 19:23:39 +0200

    Clean generic-section code

--- src/app/home/sections/generic-section/generic-section.component.ts
+++ src/app/home/sections/generic-section/generic-section.component.ts
@@ -8,7 +8,6 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GenericSectionComponent implements OnInit {
     @Input() anchor:string;
     constructor() { }
-
     ngOnInit() {
     }
 
