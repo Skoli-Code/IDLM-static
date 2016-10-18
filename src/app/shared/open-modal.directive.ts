19579610fdca8db90b801eabb5f813b52a4216a2
pbellon <bellon.pierre@gmail.com>
2016-10-18 18:28:15 +0200

    Make openModal directive work

--- src/app/shared/open-modal.directive.ts
+++ src/app/shared/open-modal.directive.ts
@@ -1,25 +1,26 @@
-import { Input, Directive, ElementRef, HostListenner } from '@angular/core';
+import { Input, Directive, ElementRef, HostListener } from '@angular/core';
 
 type windowSize = [number, number];
 @Directive({
     selector: '[idlmOpenModal]'
 })
 export class OpenModalDirective {
+    private _el:Element;
+
     @Input('idlmOpenModal') href:string;
     @Input() windowSize:windowSize = [500, 400];
 
-    constructor(private el: ElementRef, private renderer:Renderer) {
+    constructor(private el: ElementRef) {
         this._el = el.nativeElement;
-        console.log('href:', this.href);
     }
 
-    @HostListenner()
+    @HostListener('click', ['$event'])
     onClick(e){
         e.preventDefault();
-        let wname = this._el.classlist[1];
+        let wname = this._el.classList[1];
         let ww = window.innerWidth;
         let wh = window.innerHeight;
-        let w = this.windowSize[0;
+        let w = this.windowSize[0];
         let h = this.windowSize[1];
         let wstyle = `
             height=${h},width=${w},top=${(wh/2)-h/2},left=${(ww/2)-(w/2)},
