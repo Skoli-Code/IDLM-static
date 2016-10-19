b8615c173df02ee43ded09f2c0d928f329cffe02
pbellon <bellon.pierre@gmail.com>
2016-10-19 15:51:12 +0200

    Complete the informations used for social medias

--- src/app/home/step-navigation/socials.ts
+++ src/app/home/step-navigation/socials.ts
@@ -1,5 +1,7 @@
 import 'jquery';
 
+let _enc_ = encodeURIComponent;
+
 function getTitle(){
     return $('head title').text();
 }
@@ -13,15 +15,22 @@ export interface Socials {
     facebook: string,
     twitter: string
 }
-
+let meta = {
+    url: _enc_('http://islam-objet.mediatique.fr'),
+    twitter: {
+        tweet: _enc_('"L\'islam, objet médiatique” - Étude sur le traitement médiatique de l\'islam dans la presse française'),
+        hashtags: ['islam', 'dataviz'].join(',')
+    },
+    linkedin: {
+        title: _enc_('“L\'islam, objet médiatique” - Étude sur le traitement médiatique de l’islam dans la presse française (1997-2015)'),
+        summary: _enc_('Analyse de données textuelles et datavisualisations sur le traitement de l\'islam dans la presse, à partir des archives de trois grands quotidiens. Par Skoli et M.Bourekba.'),
+        source: _enc_('Skoli')
+    }
+}
 export function initSocials(){
-    let title = encodeURIComponent(getTitle());
-    let url   = encodeURIComponent(getProp('og:url'));
-    let desc  = encodeURIComponent(getProp('description'));
-    let tweet = encodeURIComponent(getProp('tweet_text'));
     return {
-        linkedin: `https://www.linkedin.com/shareArticle?url=${url}&title=${title}&summary=${desc}&mini=true`,
-        twitter: `https://twitter.com/intent/tweet?text=${tweet}&url=${url}&via=Agence-Skoli`,
-        facebook: `https://facebook.com/sharer.php?url=${url}`
+        linkedin: `https://www.linkedin.com/shareArticle?url=${meta.url}&source=${meta.linkedin.source}&title=${meta.linkedin.title}&summary=${meta.linkedin.summary}&mini=true`,
+        twitter: `https://twitter.com/intent/tweet?text=${meta.twitter.tweet}&url=${meta.url}&hastags=${meta.twitter.hashtags}`,
+        facebook: `https://facebook.com/sharer.php?url=${meta.url}`
     }
 }
