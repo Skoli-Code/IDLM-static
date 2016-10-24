11a17b5e2abc8b6d798ea360f18087dd68e53858
pbellon <bellon.pierre@gmail.com>
2016-10-24 18:31:53 +0200

    Add padding top for about & methodologie parts
    Also improve links on those parts & include methodologie content #7

--- src/app/home/sections/generic-section/generic-section.component.ts
+++ src/app/home/sections/generic-section/generic-section.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GenericSectionComponent implements OnInit {
     @Input() anchor:string;
+    @Input() topPadding:boolean=false;
     constructor() { }
     ngOnInit() {
     }
