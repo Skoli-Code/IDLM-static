cefad296d9aabbd9a00afd04720e8baacfeeed8d
pbellon <bellon.pierre@gmail.com>
2016-10-25 12:04:30 +0200

    Add web-animation lib as vendor script.
    This should make work angular-animations work everywhere. Which is nice
    :shipit:.

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -10,22 +10,18 @@ var defaultDuration:number=200;
 export var fixedWidthFadeRight = trigger('fixedWidthFadeRight', [
     transition('void => *', [
         style({
-            width: '400px', opacity: 0, transform: 'translate(-100%, 0)',
-            '-webkit-transform': 'translate(-100%, 0)'
+            width: '400px', opacity: 0, transform: 'translate(-100%, 0)'
         }),
         animate(200, style({
-            opacity: 1, transform: 'translate(0%, 0)',
-            '-webkit-transform': 'translate(0%, 0)'
+            opacity: 1, transform: 'translate(0%, 0)'
         }))
     ]),
     transition('* => void', [
         style({
-            position: 'absolute', width: '400px', opacity: 1, transform: 'translate(0%, 0)',
-            '-webkit-transform': 'translate(0%, 0)'
+            position: 'absolute', width: '400px', opacity: 1, transform: 'translate(0%, 0)'
         }),
         animate(200, style({
-            opacity: 0, transform: 'translate(100%, 0)',
-            '-webkit-transform': 'translate(100%, 0)'
+            opacity: 0, transform: 'translate(100%, 0)'
         }))
     ]),
 ]);
@@ -46,32 +42,26 @@ export function fadeDown(duration:number=defaultDuration){
     return trigger('fadeDown', [
         transition('void => *', [
             style({
-                position:'absolute', opacity: 0, transform: 'translateY(-100%)',
-                '-webkit-transform': 'translateY(-100%)'
+                position:'absolute', opacity: 0, transform: 'translateY(-100%)'
             }),
             animate(duration, style({
-                opacity: 1, transform: 'translateY(0)',
-                '-webkit-transform': 'translateY(0)'
+                opacity: 1, transform: 'translateY(0)'
             }))
         ]),
         transition('* => *', [
             style({
-                position:'absolute', opacity: 0, transform: 'translateY(-100%)',
-                '-webkit-transform': 'translateY(-100%)'
+                position:'absolute', opacity: 0, transform: 'translateY(-100%)'
             }),
             animate(duration, style({
-                opacity: 1, transform: 'translateY(0)',
-                '-webkit-transform': 'translateY(0)'
+                opacity: 1, transform: 'translateY(0)'
             }))
         ]),
         transition('* => void', [
             style({
-                position:'absolute',opacity: 1, transform: 'translateY(0%)',
-                '-webkit-transform': 'translateY(0%)'
+                position:'absolute',opacity: 1, transform: 'translateY(0%)'
             }),
             animate(duration, style({
-                opacity: 0, transform: 'translateY(100%)',
-                '-webkit-transform': 'translateY(100%)'
+                opacity: 0, transform: 'translateY(100%)'
             }))
         ])
     ]);
@@ -82,22 +72,18 @@ export function slideRight(duration:number=defaultDuration){
     return trigger('slideRight', [
         transition('void => *', [
             style({
-                transform: 'translateX(100%)',
-                '-webkit-transform': 'translateX(100%)'
+                transform: 'translateX(100%)'
             }),
             animate(duration, style({
-                transform: 'translateX(0%)',
-                '-webkit-transform': 'translateX(0%)'
+                transform: 'translateX(0%)'
             }))
         ]),
         transition('* => void', [
             style({
-                transform: 'translateX(0%)',
-                '-webkit-transform': 'translateX(0%)'
+                transform: 'translateX(0%)'
             }),
             animate(duration, style({
-                transform: 'translateX(100%)',
-                '-webkit-transform': 'translateX(100%)'
+                transform: 'translateX(100%)'
             }))
         ])
     ]);
